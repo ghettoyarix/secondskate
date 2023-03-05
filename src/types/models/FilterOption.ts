f2e90e3fe06685b375304117dd9b1bb394aee56a
xpucmoc51 <konigsegger@gmail.com>
2023-03-05 15:54:49 +0300

    Sync

---
+++ src/types/models/FilterOption.ts
@@ -0,0 +1,11 @@
+import { StringLike } from "@firebase/util";
+
+type FilterOption = {
+    value : string;
+    title : string;
+};
+type CategoryOption = {
+    title: string;
+    
+};
+export default FilterOption;
